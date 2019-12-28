3ae57eb6d91bbc2732e04f9251ca70335bd0309e
jenross <ross.m.jennifer@gmail.com>
2019-12-28 16:58:47 -0500

    added functions for displaying covers

--- src/App.js
+++ src/App.js
@@ -10,6 +10,36 @@ function Carousel(props) {
   return <section className="Carousel" {...props} />;
 }
 
+function Covers(props) {
+  return <ul {...props} />;
+}
+
+function EachCover({ isCurrent, takeFocus, image, id, title, children }) {
+  let ref = useRef();
+
+  useEffect(() => {
+    if (isCurrent && takeFocus) {
+      ref.current.focus();
+    }
+  }, [isCurrent, takeFocus]);
+
+  return (
+    <li
+      ref={ref}
+      aria-hidden={!isCurrent}
+      tabIndex="-1"
+      aria-labelledby={id}
+      className="Slide"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <div className="SlideContent">
+        <h2 id={id}>{title}</h2>
+        {children}
+      </div>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
