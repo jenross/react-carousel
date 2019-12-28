a1b8ca9210d0b3b855de2be389e2932d183b565d
jenross <ross.m.jennifer@gmail.com>
2019-12-28 16:49:22 -0500

    eslint and prettier config; imported album covers and code for prog bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer, useEffect, useRef } from "react";
+import { FaPlay, FaPause, FaForward, FaBackward } from "react-icons/fa";
+import Albums from "./components/Albums";
+import useProgress from "./components/useProgress";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/components/Albums.js
@@ -0,0 +1,67 @@
+import React from "react";
+import betteroblivion from "../assets/betteroblivion";
+import boniver from "../assets/boniver";
+import chance from "../assets/chance";
+import highwomen from "../assets/highwomen";
+import lizzo from "../assets/lizzo";
+import maggie from "../assets/maggie";
+import national from "../assets/national";
+import sturgill from ".../assets/sturgill";
+
+export default [
+  {
+    img: betteroblivion,
+    title: "Better Oblivion Community Center: Better Oblivion Community Center",
+    content: (
+      <p>
+        The 16-year-old Bright Eyes fan inside will never tire of Conor Oberst.
+      </p>
+    )
+  },
+  {
+    img: boniver,
+    title: "Bon Iver: I, I",
+    content: <p>Always different, always fascinating.</p>
+  },
+  {
+    img: chance,
+    title: "Chance the Rapper: The Big Day",
+    content: (
+      <p>
+        "Look at him, the kid's got big, big plans! And I hope they all work
+        out, I really do. But, do you ever really think about how it's s'posed
+        to all work out? Do you have health insurance? A retirement plan? And
+        you need to start savin' money, right? Huh?...And you need to pay me
+        that money you owe me. And you need a wife. And you need Jesus. Oh, and
+        you need to be a vegan. Don't forget about that veganism, that's that
+        2019, it's almost 2020. You need to stop doin' that Percy pill. And he
+        gon' need a car, son."
+      </p>
+    )
+  },
+  {
+    img: highwomen,
+    title: "The Highwomen: The Highwomen",
+    content: <p>Everything Brandi Carlile touches...</p>
+  },
+  {
+    img: lizzo,
+    title: "Lizzo: Cuz I Love You",
+    content: <p>No explanation necessary.</p>
+  },
+  {
+    img: maggie,
+    title: "Maggie Rogers: Heard it in a Past Life",
+    content: <p>Angel</p>
+  },
+  {
+    img: national,
+    title: "The National: I am Easy to Find",
+    content: <p>Haunting and beautiful, as always.</p>
+  },
+  {
+    img: sturgill,
+    title: "Sturgill Simpson: Sound & Fury",
+    content: <p>Make art not friends.</p>
+  }
+];
